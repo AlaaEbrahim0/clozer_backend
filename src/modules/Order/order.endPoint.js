c598720ea081f0368e50eeb0e313191135569048
kimoo tareek <kimootareekk@outlook.com>
2024-03-18 15:59:18 +0200

    test auth

--- src/modules/Order/order.endPoint.js
+++ src/modules/Order/order.endPoint.js
@@ -1,10 +1,12 @@
-import { roles } from "../../middleware/auth.js"
+import { roles } from "../../middleware/auth.js";
 
-const orderEndPoint ={
-    create:[roles.User],
-    cancel:[roles.User],
-    rejected:[roles.User],
-    delivered:[roles.User]//change to admin
-
-}
-export default orderEndPoint
+const orderEndPoint = {
+  create: [roles.User],
+  get: [roles.User, roles.Admin],
+  All: [roles.Admin],
+  getById: [roles.Admin, roles.User],
+  cancel: [roles.User],
+  rejected: [roles.User],
+  delivered: [roles.User], //change to admin
+};
+export default orderEndPoint;
